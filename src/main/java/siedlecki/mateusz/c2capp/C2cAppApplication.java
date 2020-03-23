b380a3f8829becfc5adbf2b89feb4b0f62656583
Mateusz <siedleckimateus@gmail.com>
2020-03-23 17:27:46 +0100

    first commit

---
+++ src/main/java/siedlecki/mateusz/c2capp/C2cAppApplication.java
@@ -0,0 +1,13 @@
+package siedlecki.mateusz.c2capp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2cAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C2cAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/siedlecki/mateusz/c2capp/C2cAppApplicationTests.java
@@ -0,0 +1,13 @@
+package siedlecki.mateusz.c2capp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2cAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
